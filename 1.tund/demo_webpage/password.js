3785783f9229b0570c0be4470f8d657373df0369
Norman J <npjyrlau@gmail.com>
2022-09-29 17:34:27 +0300

    1.3.2

---
+++ 1.tund/demo_webpage/password.js
@@ -0,0 +1,10 @@
+/*ka ei tööta
+function done() {
+    document.getElementById("popup").style.display = "none";
+    var password = document.getElementById("pass").value;
+};
+
+function showPopup() {
+    document.getElementById("popup").style.display = "block";
+}
+*/
